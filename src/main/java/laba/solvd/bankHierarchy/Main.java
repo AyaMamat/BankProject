767fae77a86411bf25121853ce932af2d5b233c5
AyaMamat <amamat.laba@solvd.com>
2023-11-14 06:09:57 -0600

    last

--- src/main/java/laba/solvd/bankHierarchy/Main.java
+++ src/main/java/laba/solvd/bankHierarchy/Main.java
@@ -17,11 +17,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-
     private static final Logger logger = LogManager.getLogger(Main.class);
-
     public static void main(String[] args) {
-
 
         // Creating a bank
         Bank bank = new Bank("Capital One");
