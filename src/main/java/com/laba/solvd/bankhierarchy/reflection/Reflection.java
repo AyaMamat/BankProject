4dfa99dc5cc62bc58bb3f13a3d7ba6ebb57adfe9
AyaMamat <amamat.laba@solvd.com>
2023-12-04 22:00:14 -0600

    reflection

--- src/main/java/com/laba/solvd/bankhierarchy/reflection/Reflection.java
+++ src/main/java/com/laba/solvd/bankhierarchy/reflection/Reflection.java
@@ -4,13 +4,31 @@ import com.laba.solvd.bankhierarchy.people.Customer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+
 public class Reflection {
     private static final Logger LOGGER = LogManager.getLogger(Reflection.class);
 
+    public static void main(String[] args) {
+        reflection();
+    }
     public static void reflection(){
 
+    try{
         Customer customer=new Customer("Kayla Kobe", "1234 Quentin Rd","123 455 6664" );
 
+        LOGGER.info("FIELDS");
+        List<Field>customerFields= Arrays.asList(customer.getClass().getDeclaredFields());
+        customerFields.stream().forEach(field -> LOGGER.info(field.getName() + " -->> "+ Modifier.toString(field.getModifiers())));
 
+        LOGGER.info("METHODS");
+        List<Method>customerMethods= Arrays.asList(customer.getClass().getDeclaredMethods());
+        customerMethods.stream().forEach(method -> LOGGER.info(method.getName() + " -->> "+ Modifier.toString(method.getModifiers())));
+    } catch(Exception e){
+        LOGGER.error("Error has ");
     }
 }
